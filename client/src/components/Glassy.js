75256ce3a2e08898cf1e4c648e871f37ca071d7b
Tanakrit Maenphol <Thanagrith@gmail.com>
2025-01-22 15:10:29 +0700

    lil progress of projects commit && create glassy components

--- client/src/components/Glassy.js
+++ client/src/components/Glassy.js
@@ -1,9 +1,16 @@
 import './Glassy.css';
 
-function Glassy({ Children }) {
+function Glassy({ children }) {
   return (
-    <div className='glassy'>
-      {Children}
+    <div className='glassy-container'>
+      <div className='inner-glassy-container'>
+        <div className='title'>
+          {children.props.title}
+        </div>
+        <div className='content'>
+          {children.props.children}
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Menu from '../components/Menu';
 import Port from '../components/Port';
 import Chat from '../components/Chat';
+import Glassy from '../components/Glassy';
 
 function App() {
   return (
@@ -15,8 +16,36 @@ function App() {
         </header>
       </div>
       <div className='linear-line' />
-      <div className='projects'>
-        Projects
+      <div className='projects-container'>
+        <div className='inner-projects-container'>
+          <div className='projects-text'>Projects</div>
+          <div className='glassy-box'>
+            <div className='left-project'>
+              <Glassy>
+                <div title="AgentDev">This is content</div>
+              </Glassy>
+            </div>
+            <div className='center-project'>
+              <div className='center-top'>
+                <Glassy>
+                  <div title="AgentDev">This is content</div>
+                </Glassy>
+              </div>
+              <div className='center-bottom'>
+                <Glassy>
+                  <div title="AgentDev">This is content</div>
+                </Glassy>
+              </div>
+            </div>
+            <div className='right-project'>
+              <Glassy>
+                <div title="AgentDev">This is content</div>
+              </Glassy>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='skills-container'>
       </div>
     </div>
   );
